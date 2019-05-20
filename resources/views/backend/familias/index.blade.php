1bf11747c933fe84062e5b1e887e8d1af4d839b5
TianRB <tianrb@gmail.com>
2019-05-20 14:05:32 -0500

    Modelo, vistas y controladores para Familias

---
+++ resources/views/backend/familias/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="panel panel-default">
+        <div class="panel-heading">Dashboard</div>
+        
+        <div class="panel-body">
+          @if (session('status'))
+          <div class="alert alert-success">
+            {{ session('status') }}
+          </div>
+          @endif
+          <p>Index Familias</p>
+          <a href=" {{ route('familias.create')}} ">create</a>
+          <ul>
+            @forelse ($familias as $f)
+            {{-- var_dump($f) --}}
+            <p>{{ $f->display_name }}</p>
+            <a href="{{ route('familias.show', $f->id) }}">show</a>
+            <a href="{{ route('familias.edit', $f->id) }}">edit</a>
+            <form action="{{ route('familias.destroy', ['id' => $f->id]) }}" method="POST" class="no-margin">
+              {{ csrf_field() }}
+              <input type="hidden" name="_method" value="delete" />
+              <button type="submit" class="btn btn-danger"><i class="fa fa-trash">delete</i></button>
+            </form>
+            @empty
+              <p>Tabla Vacía</p>
+            @endforelse
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
