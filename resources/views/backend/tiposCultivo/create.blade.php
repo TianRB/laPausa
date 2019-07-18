9bd266a6a8062f1ab2426e04472db7eda46a3e5e
Arturo Rosales <arturoi_rosales@hotmail.com>
2019-07-18 11:55:48 -0500

    123

--- resources/views/backend/tiposCultivo/create.blade.php
+++ resources/views/backend/tiposCultivo/create.blade.php
@@ -1,33 +1,40 @@
 @extends('layouts.dashboard')
+@section('page_title')
+  Crear tipo de cultivo
+@endsection
+
 
 @section('content')
 <div class="container">
+    @if (session('status'))
+    <div class="alert alert-success">
+      {{ session('status') }}
+    </div>
+    @endif
   <div class="row">
-    <div class="col-md-8 col-md1-offset-2">
-      <div class="panel panel-default">
-        <div class="panel-heading">Dashboard</div>
+    <div class="col-12">
+        <h1 class="">@yield('page_title')</h1>
+    </div>
+  </div>
+  <div class="row">
+
+    <div class="col-12">
+      <div class="panel-body">
+   
+        <p>Create Tipos de Cultivo</p>
+        <a href=" {{ route('tipos_cultivo.index')}} ">index</a>
         
-        <div class="panel-body">
-          @if (session('status'))
-          <div class="alert alert-success">
-            {{ session('status') }}
-          </div>
-          @endif
-          <p>Create Tipos de Cultivo</p>
-          <a href=" {{ route('tipos_cultivo.index')}} ">index</a>
-          
-          <form action="{{ route('tipos_cultivo.store') }}" method="post">
-            {{ csrf_field() }}
-            <label for="display_name">display_name</label>
-            <input type="text" name="display_name" value="{{ old('display_name') }}">
+        <form action="{{ route('tipos_cultivo.store') }}" method="post">
+          {{ csrf_field() }}
+          <label for="display_name">display_name</label>
+          <input type="text" name="display_name" value="{{ old('display_name') }}">
 
-            <label for="description">description (optional)</label>
-            <input type="text" name="description" value="{{ old('description') }}">
+          <label for="description">description (optional)</label>
+          <input type="text" name="description" value="{{ old('description') }}">
 
-            <input type="submit" value="submit">
-          </form>
+          <input type="submit" value="submit">
+        </form>
 
-        </div>
       </div>
     </div>
   </div>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,7 +8,7 @@
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
       
-      <title>La Pausa</title>
+      <title>La Pausa | @yield('page_title')</title>
       
       <!-- Bootstrap 4 -->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -26,25 +26,8 @@
                 </div>
               </nav>
           
-              <main role="main" class="col-md-9 ml-sm-auto col-lg-10 bg-5">
-                <div class="row">
-                  <nav class="col-12 py-2 bg-light"> 
-                    <ul class="nav d-flex justify-content-end">
-                      <li class="nav-item">
-                        <a class="nav-link active" href="#">Active</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
+              <main role="main" class="col-lg-10 bg-5">
+                @include('layouts.dashboard_navbar')
                  {{-- Content --}}
                 <div class="row justify-content-center py-5">                                 
                  <div class="col-11">
@@ -56,6 +39,9 @@
           </div>
 
     <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
