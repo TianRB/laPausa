9c2b6605fcf58ec3645d72fcf94c32f5f225f7e6
TianRB <tianrb@gmail.com>
2019-07-26 16:02:21 -0500

    Update show.blade.php

--- resources/views/backend/siembraHongos/show.blade.php
+++ resources/views/backend/siembraHongos/show.blade.php
@@ -14,12 +14,12 @@
           </div>
           @endif
           <p>show Tipos de Cultivo</p>
-          <a href=" {{ route('tipos_cultivo.index')}} ">index</a>
+          <a href=" {{ route('siembra_hongo.index')}} ">index</a>
           <ul>
             <p>{{ $siembraHongo->display_name }}</p>
-            <a href="{{ route('tipos_cultivo.show', $siembraHongo->id) }}">show</a>
-            <a href="{{ route('tipos_cultivo.edit', $siembraHongo->id) }}">edit</a>
-            <form action="{{ route('tipos_cultivo.destroy', ['id' => $siembraHongo->id]) }}" method="POST" class="no-margin">
+            <a href="{{ route('siembra_hongo.show', $siembraHongo->id) }}">show</a>
+            <a href="{{ route('siembra_hongo.edit', $siembraHongo->id) }}">edit</a>
+            <form action="{{ route('siembra_hongo.destroy', ['id' => $siembraHongo->id]) }}" method="POST" class="no-margin">
               {{ csrf_field() }}
               <input type="hidden" name="_method" value="delete" />
               <button type="submit" class="btn btn-danger"><i class="fa fa-trash">delete</i></button>
